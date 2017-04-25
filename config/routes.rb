983cfa04b99169f8d5e733376cd300e212cc97ef
dima12345 <gunosada1@gmail.com>
2017-04-25 23:53:13 +0300

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # require 'sidekiq/web'
 Rails.application.routes.draw do
+  devise_for :users
+  get 'persons/profile'
+
+  root to: 'home#index'
+  get 'home/index', to: 'home#index'
+  get 'persons/profile', as: 'user_root'
   # mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
